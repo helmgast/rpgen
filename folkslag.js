8e07d28cdced5036532e7edf7b52531031250758
avatarex <anton@helmgast.se>
2016-12-22 23:06:55 +0100

    Update folkslag.js

--- folkslag.js
+++ folkslag.js
@@ -27,7 +27,8 @@
 	//var folkslaglista = ["zolod"];
 	var folkslagobjekt={};
 	folkslagobjekt.lista=folkslaglista;
-	
+	folkslagobjekt.rubriker=["adasier", "cermirier", "cirefalier", "fedaki", "genver", "jahann", "jargier", "kamorian", "marnakhtirak", "missla", "pyar", "taup", "utmarkare", "zolod"];
+
 	fardighetslistaobjekt = hamta_fardighetslistaobjekt(); // Behövs för zolods 5T6 Hantverksfärdighet
 	hantverkfardigheter=fardighetslistaobjekt.hantverkfardigheter.lista;
 	fardighetsobjekt=hamta_fardighetsobjekt();
